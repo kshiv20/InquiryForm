5cfa67e07c7c9825891006ee3c4bcee4a57880e8
Ashish Srivastava <shivkumarmandal13@gmail.com>
2023-12-20 18:01:46 +0530

    Dev Done

---
+++ js/script.js
@@ -0,0 +1,23 @@
+// function validateForm() {
+//     let firstName = document.forms["myForm"]["fname"].value;
+//     let lastName = document.forms["myForm"]["lname"].value;
+//     let email = document.forms["myForm"]["email"].value;
+//     let phoneNumber = document.forms["myForm"]["pnumber"].value;
+//     if (firstName == "") {
+//       alert("Name must be filled out");
+//       return false;
+//     } 
+//     else if (lastName == "") {
+//         alert("Last name must be filled out");
+//       return false;
+//     }
+//     else if (email == ""){
+//         alert("Email must be filled out");
+//         return false;
+//     }
+//     else if (phoneNumber == ""){
+
+//         alert("Phone Number must be filled out");
+//         return false;
+//     }
+//   }
